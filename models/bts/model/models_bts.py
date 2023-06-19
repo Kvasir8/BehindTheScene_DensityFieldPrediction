fixed issue for torch matrix computation for masking invalid handler for color

models/bts/model/models_bts.py
@@ -200,7 +200,7 @@ class MVBTSNet(torch.nn.Module):
             invalid = torch.any(invalid, dim=1) ## sampled_features are averaged into the same dim with single featuremap
         return sampled_features, invalid[..., 0].permute(0, 2, 1)    ## !! The output of the function is a tuple containing the sampled features and a boolean tensor indicating the invalid features
 
-    def sample_colors(self, xyz):   ## ? where does z come from? we're working on image domain with predicted depth from density field computed?
+    def sample_colors(self, xyz):
         n, n_pts, _ = xyz.shape                     ## n := batch size, n_pts := #_points in world coord.
         n, nv, c, h, w = self.grid_c_imgs.shape     ## nv := #_views
         xyz = xyz.unsqueeze(1)                      # (n, 1, pts, 3)

models/bts/trainer.py
@@ -87,7 +87,7 @@ class BTSWrapper(nn.Module):
 
     def forward(self, data):
         data = dict(data)
-        images = torch.stack(data["imgs"], dim=1)                           # n, v, c, h, w
+        images = torch.stack(data["imgs"], dim=1)                           # n, v, c, h, w     ## ? v:= patch_size??
         poses = torch.stack(data["poses"], dim=1)                           # n, v, 4, 4 w2c
         projs = torch.stack(data["projs"], dim=1)                           # n, v, 4, 4 (-1, 1)
 
@@ -207,6 +207,7 @@ class BTSWrapper(nn.Module):
 
         with profiler.record_function("trainer_encode-grid"):
             self.renderer.net.compute_grid_transforms(projs[:, ids_encoder], poses[:, ids_encoder])
+            self.renderer.net.encode(images, projs, poses, ids_encoder=ids_encoder, ids_render=ids_render, images_alt=images_ip, combine_ids=combine_ids)
 
         sampler = self.train_sampler if self.training else self.val_sampler
 